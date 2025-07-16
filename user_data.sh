248f4b3dde3e0a7bcbe7d6b120145cf54916726f
thoibi3 <thoibi3@gmail.com>
2025-07-16 13:36:26 +0530

    fix

--- user_data.sh
+++ user_data.sh
@@ -1,32 +1,3 @@
-# #!/bin/bash
-# # Update and install Python3, pip, etc.
-# apt update -y
-# apt install -y python3-pip
-
-# # Navigate to home directory
-# cd /home/ubuntu
-
-# # Create FastAPI application
-# cat <<EOF > main.py
-# from fastapi import FastAPI
-
-# app = FastAPI()
-
-# @app.get("/")
-# def root():
-#     return {"message": "Hello from FastAPI on Ubuntu EC2!!!!"}
-# EOF
-
-# # Install FastAPI and Uvicorn
-# pip3 install fastapi uvicorn
-
-# # Run the FastAPI app on port 80 in background
-# sudo nohup uvicorn main:app --host 0.0.0.0 --port 8000 &
-
-#!/bin/bash
-
-# Update packages and install required software
-
 apt update -y
 apt install -y git python3-pip
 
@@ -39,4 +10,4 @@ cd app
 pip3 install fastapi uvicorn
 
 # Start FastAPI in background on port 80
-nohup uvicorn main:app --host 0.0.0.0 --port 80 > fastapi.log 2>&1 &
+nohup uvicorn main:app --host 0.0.0.0 --port 8000 > fastapi.log 2>&1 &
