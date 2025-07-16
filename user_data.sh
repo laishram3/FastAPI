563b6b14dfcc41fe6a312707ecd223b941782d37
thoibi3 <thoibi3@gmail.com>
2025-07-16 15:19:01 +0530

    updatee

--- user_data.sh
+++ user_data.sh
@@ -9,5 +9,5 @@ cd app
 # Install FastAPI and Uvicorn
 pip3 install fastapi uvicorn boto3
 
-# Start FastAPI in background on port 80
-# nohup uvicorn main:app --host 0.0.0.0 --port 8000 > fastapi.log 2>&1 &
+# Start FastAPI in background on port 8000
+nohup uvicorn main:app --host 0.0.0.0 --port 8000 > fastapi.log 2>&1 &
