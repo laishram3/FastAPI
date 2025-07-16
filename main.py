330e6a2f3da929ad73ec5d454819e5e0ddb2d37e
thoibi3 <thoibi3@gmail.com>
2025-07-16 15:48:33 +0530

    fi

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
 from fastapi import FastAPI
-import boto3
 
 app = FastAPI()
 
-
-dynamodb = boto3.resource("dynamodb", region_name="us-east-1")
-table = dynamodb.Table("DemoTab")
-
 @app.get("/")
 def read_root():
-
-    response = table.scan()
-    return response.get("Items", [])
-    
+    return {"Hello": "World"}
