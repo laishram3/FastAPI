44a6d03bee204ea3effed4b2319eb0c4c3e68c47
laishram3 <laishram@gmail.com>
2025-07-18 18:01:52 +0530

    Initial check in

---
+++ user_data.sh
@@ -0,0 +1,13 @@
+apt update -y
+apt install -y git python3-pip
+
+# Clone Bitbucket repo (public repo example)
+cd /home/ubuntu
+git clone https://bitbucket.org/thoibi3/FastApiEc2.git app
+cd app
+
+# Install FastAPI and Uvicorn
+pip3 install fastapi uvicorn boto3
+
+# Start FastAPI in background on port 8000
+nohup uvicorn main:app --host 0.0.0.0 --port 8000 > fastapi.log 2>&1 &
