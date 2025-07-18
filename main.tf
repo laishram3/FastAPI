8f60720260c2618f6ea9e2dcfa4749d47ef3f17e
laishram3 <laishram@gmail.com>
2025-07-18 19:48:57 +0530

    code checkin

--- main.tf
+++ main.tf
@@ -11,23 +11,65 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  # profile = "default"
+  #profile = "default"
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
+# 1. Create VPC
+resource "aws_vpc" "main_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
+  tags = {
+    Name = "MyVPC"
   }
 }
 
-resource "aws_security_group" "fastapi_sg" {
-  name        = "fastapi-sg"
-  description = "Allow HTTP and SSH traffic"
+# 2. Create Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "MyInternetGateway"
+  }
+}
+
+# 3. Create Public Subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true # 🔸 This makes it a PUBLIC subnet
+
+  tags = {
+    Name = "PublicSubnet"
+  }
+}
+
+# 4. Create Route Table for Public Subnet
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "PublicRouteTable"
+  }
+}
+
+# 5. Associate Route Table with Public Subnet
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# 6. Create Security Group
+resource "aws_security_group" "public_sg" {
+  name        = "PublicSG"
+  description = "Allow SSH and HTTP"
+  vpc_id      = aws_vpc.main_vpc.id
 
   ingress {
     from_port   = 8000
@@ -59,26 +101,29 @@ resource "aws_security_group" "fastapi_sg" {
   }
 }
 
-
-resource "aws_instance" "fastapi_server" {
-  ami                         = "ami-053b0d53c279acc90"
-  instance_type               = "t2.micro"
-  key_name                    = aws_key_pair.deployer.key_name
-  security_groups             = [aws_security_group.fastapi_sg.name]
-  associate_public_ip_address = true
-  user_data                   = file("user_data.sh")
-  iam_instance_profile        = "ec2-dynamodb-instance-profile"
-  subnet_id                   = tolist(data.aws_subnets.default.ids)[0]
-  tags = {
-    Name = "FastAPI-Server"
-  }
-}
-
-resource "aws_key_pair" "deployer" {
+# 7. Create Key Pair (for SSH access)
+resource "aws_key_pair" "my_key" {
   key_name   = "id_rsa"
   public_key = file("${path.module}/id_rsa.pub")
 }
 
+# 8. Launch EC2 in Public Subnet
+resource "aws_instance" "public_ec2" {
+  ami                         = "ami-053b0d53c279acc90" # Ubuntu 22.04 (us-east-1)
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public_subnet.id
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.my_key.key_name
+  security_groups             = [aws_security_group.public_sg.id]
+
+  tags = {
+    Name = "PublicDemoEC2"
+  }
+
+  user_data                   = file("user_data.sh")
+}
+
+
 output "instance_public_ip" {
-  value = aws_instance.fastapi_server.public_ip
+  value = aws_instance.public_ec2.public_ip
 }
