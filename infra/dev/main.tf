8b02119ba0c378be656ec3e21979073eb7c3eaf9
laishram3 <laishram@gmail.com>
2025-07-18 20:25:58 +0530

    fix build

--- infra/dev/main.tf
+++ infra/dev/main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+  profile = "default"
+}
+
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   tags = {
