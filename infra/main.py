01cc8120ecac7df030ef8ed024316ae1f44cfaea
laishram3 <laishram@gmail.com>
2025-07-19 06:26:46 +0530

    fix

---
+++ infra/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+import boto3
+
+app = FastAPI()
+
+
+dynamodb = boto3.resource("dynamodb", region_name="us-east-1")
+table = dynamodb.Table("DemoTab")
+
+@app.get("/")
+def read_root():
+
+    response = table.scan()
+    return response.get("Items", [])
